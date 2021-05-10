d0ab9ee0f492c008c681801b02ed4d7be6889450
DGnzl <dnlgonzal@outlook.com>
2021-05-09 23:52:46 -0700

    Greeting code added

---
+++ FormattedString.cs
@@ -0,0 +1,11 @@
+using System;
+namespace CodeWars
+{
+    public static class FormatS
+    {
+        public static string Greet(string name)
+        {
+            return $"Hello, {name} how are you doing today?";
+        }
+    }
+}
